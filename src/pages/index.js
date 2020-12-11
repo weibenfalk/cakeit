9a29b62f9d3d7963e434f72d239c306b290ebf66
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2020-12-11 11:31:19 +0100

    index - Quote

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout/Layout"
 import Hero from "../components/Hero/Hero"
 import CTAArea from "../components/CTAArea/CTAArea"
 import LatestBlogPost from "../components/LatestBlogPost/LatestBlogPost"
+import Quote from "../components/Quote/Quote"
 
 const IndexPage = () => (
   <Layout>
@@ -11,6 +12,7 @@ const IndexPage = () => (
     <Hero />
     <CTAArea />
     <LatestBlogPost />
+    <Quote />
   </Layout>
 )
 
