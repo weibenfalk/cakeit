04820356a71fe4961858643a152572a1174cc115
Thomas Weibenfalk <thomas.weibenfalk@gmail.com>
2020-12-11 13:07:57 +0100

    Pages - Scaffolding

--- src/pages/{wpPage.link}.js
+++ src/pages/{wpPage.link}.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react"
+import { graphql } from "gatsby"
+import styled from "styled-components"
+// Components
+import Layout from "../components/Layout/Layout"
+
+const Wrapper = styled.div`
+  max-width: 1180px;
+  margin: 0 auto;
+  padding: 20px;
+`
+
+const ContentWrapper = styled.div`
+  display: block;
+
+  @media (min-width: 992px) {
+    display: flex;
+  }
+`
+
+const PageContent = styled.article`
+  margin-top: 20px;
+`
 
 const PageTemplate = () => (
-  <div>
-    <p>Page</p>
-  </div>
+  <Layout>
+    <p>PageHero</p>
+    <Wrapper>
+      <p>Sidebar</p>
+      <p>Content</p>
+    </Wrapper>
+  </Layout>
 )
 
 export default PageTemplate
